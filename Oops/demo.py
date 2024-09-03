8db9828bb5368f7ab27d99bafa61723f0969f1fa
Sneharag <sneharag101@gmail.com>
2024-09-03 21:32:39 +0530

    oopsdemo

--- Oops/demo.py
+++ Oops/demo.py
@@ -1,2 +1,31 @@
-print("Object Oriented Programming")
-print("Hello World")
+
+# class [design pattern,template,blueprint]
+# object | instance [real worl entity]
+
+class student:
+
+    name:str
+    age:int
+    gender:str
+    contact:int
+    course:str
+
+    def set_stud(self,name,age,gender,contact,course):
+
+        self.name=name
+        self.age=age
+        self.gender=gender
+        self.contact=contact
+        self.course=course
+
+    def display_stud(self):
+
+        print(self.name,self.age,self.gender,self.contact,self.course)
+
+
+# creating objects
+
+stud_obj=student()
+
+stud_obj.set_stud("hari",20,"male",4567890,"django developer")
+stud_obj.display_stud()
